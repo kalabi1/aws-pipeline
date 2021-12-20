094cbb83cb2a7d8188f3b3a987ff156cad2e9a7e
kenny <alabikehinde316@gmail.com>
2021-12-20 20:23:36 +0000

    Modifying

--- bucket-module/s3-backend.tf
+++ bucket-module/s3-backend.tf
@@ -1,5 +1,9 @@
-resource "aws_s3_bucket" "backend-s3-tf" {
-  bucket = "backend-s3-tf"
+resource "aws_s3_bucket" "back-s3-tf" {
+  bucket = "back-s3-tf"
   acl    = "private"
   force_destroy = true
+}
+
+provider "aws" {
+    region = "eu-west-2"
 }
--- src-module/state.tf
+++ src-module/state.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "backend-s3-tf"
+        bucket = "back-s3-tf"
         encrypt = true
         key = "terraform.tfstate"
         region = "eu-west-2"
