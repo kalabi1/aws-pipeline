17c8f414c54e433dea90326c06fa16c5b782aa53
kenny <alabikehinde316@gmail.com>
2021-12-22 15:50:37 +0000

    fixes

--- webhooks.tf
+++ webhooks.tf
@@ -1,34 +1,34 @@
 
-# locals {
-#   SUPER_SECRET = "SUPER_SECRET"
+# # locals {
+# #   SUPER_SECRET = "SUPER_SECRET"
+# # }
+
+# # Create Pipeline Wobhook trigger
+# resource "aws_codepipeline_webhook" "test-webhook" {
+#   name            = "test-webhook"
+#   authentication  = "GITHUB_HMAC"
+#   target_action   = "Source"
+#   target_pipeline = aws_codepipeline.test-pipeline.name
+
+#   authentication_configuration {
+#     secret_token = var.github_token
+#   }
+
+#   filter {
+#     json_path    = "$.ref"
+#     match_equals = "refs/heads/{Branch}"
+#   }
 # }
 
-# Create Pipeline Wobhook trigger
-resource "aws_codepipeline_webhook" "test-webhook" {
-  name            = "test-webhook"
-  authentication  = "GITHUB_HMAC"
-  target_action   = "Source"
-  target_pipeline = aws_codepipeline.test-pipeline.name
+# # Create the CodePipeline webhook into a GitHub repository.
+# resource "github_repository_webhook" "test-webhook" {
+#   repository = var.repo_id
+#   configuration {
+#     url          = aws_codepipeline_webhook.test-webhook.url
+#     content_type = "json"
+#     insecure_ssl = true
+#     secret       = var.github_token
+#   }
 
-  authentication_configuration {
-    secret_token = var.github_token
-  }
-
-  filter {
-    json_path    = "$.ref"
-    match_equals = "refs/heads/{Branch}"
-  }
-}
-
-# Create the CodePipeline webhook into a GitHub repository.
-resource "github_repository_webhook" "test-webhook" {
-  repository = var.repo_id
-  configuration {
-    url          = aws_codepipeline_webhook.test-webhook.url
-    content_type = "json"
-    insecure_ssl = true
-    secret       = var.github_token
-  }
-
-  events = ["push"]
-}
+#   events = ["push"]
+# }
